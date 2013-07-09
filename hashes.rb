994b54e2568795fe2762851ee34f47a7ed2b0b42
Jeff Cohen <jeff@starterleague.com>
2013-07-09 11:40:02 -0500

    Add hashes.rb to the repository

---
+++ hashes.rb
@@ -0,0 +1,6 @@
+people = [{ "name" => "Jeff", "dessert" => "Ice Cream"},
+{ "name" => "Raghu", "dessert" => "Cookies"}]
+
+raghu = people[1] # => {"name"=>"Raghu", "dessert"=>"Cookies"}
+
+puts raghu["dessert"]  # => Cookies
