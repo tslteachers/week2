83e4e11ed09752ed9eb22669bfacbacb31914799
Jeff Cohen <jeff@starterleague.com>
2013-07-10 16:03:13 -0500

    Finish the Teacher challenge

--- sandbox.rb
+++ sandbox.rb
@@ -1,6 +1,14 @@
 require './student.rb'
 require './teacher.rb'
 
+# Create a Teacher instance, and
+# associate two students to the teacher.
+
+me = Teacher.new
+me.add_student("Raghu")
+me.add_student("Neal")
+puts me.student_count    # => 2
+
 # Your code goes here:
 student_1 = Student.new
 student_1.name = "Jeff"
--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,19 @@
 class Teacher
 
-  def set_name(student_name)
-    @name = student_name
+  def initialize
+    @students = []
+  end
+
+  def add_student(student_name)
+    @students.push student_name
+  end
+
+  def student_count
+    return @students.count
+  end
+
+  def set_name(teacher_name)
+    @name = teacher_name
   end
 
   def get_name
