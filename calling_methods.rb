ca8831f7182ebfef5fba636b3c50401a21d29556
Jeff Cohen <jeff@starterleague.com>
2013-07-12 09:48:17 -0500

    Practice calling methods and using conditional logic

---
+++ calling_methods.rb
@@ -0,0 +1,22 @@
+def introduce(first_name, last_name)
+  puts "Hi, my name is #{first_name.capitalize} #{last_name.capitalize}"
+end
+
+# Challenge:
+# Can you use the method defined above to introduce yourself?
+# Your code goes here:
+
+
+
+
+
+
+# def say_the_secret_word(word)
+#   puts "Try again."
+# end
+
+# say_the_secret_word("grapes")  # should respond with "Try again."
+# say_the_secret_word("carrots")  # should respond with "Try again."
+# say_the_secret_word("cookies")  # should respond with "You got it!"
+
+
---
+++ friday_methods.rb
@@ -0,0 +1,8 @@
+# Change this method definition so that it emits
+# "Hello, my name is Jeff Coeh"
+def introduce
+
+end
+
+
+puts "Hello, my name is Jeff Cohen."
